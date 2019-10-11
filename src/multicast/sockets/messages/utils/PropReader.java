b2b14be2059f80916ced0d1fb205cc80124e277f
Eduardo Silva <emf.silva@campus.fct.unl.pt>
2019-10-11 22:58:00 +0100

    Change conf format at least temporarilly. Change conf filename as requested.

--- src/multicast/sockets/messages/utils/PropReader.java
+++ src/multicast/sockets/messages/utils/PropReader.java
@@ -1,5 +1,6 @@
 package multicast.sockets.messages.utils;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.InvalidPropertiesFormatException;
@@ -8,18 +9,26 @@ import java.util.Properties;
 public class PropReader {
 	
 	public static void main(String[] args) {
-		PropReader prop = new PropReader("Untitled 1");
-		System.out.println(prop.getProperty("224.5.6.7:9000"));
-		System.out.println(prop.getProperty("SID"));
-		System.out.println(prop.getProperty("224.5.6.7:9000/SID"));
+		PropReader prop = new PropReader("res/SMCP.conf");
+		
+		System.out.println(prop.getProperty("ip"));
+		System.out.println(prop.getProperty("port"));
+		System.out.println(prop.getProperty("sid"));
+		System.out.println(prop.getProperty("sea"));
+		System.out.println(prop.getProperty("seaks"));
+		System.out.println(prop.getProperty("mode"));
+		System.out.println(prop.getProperty("padding"));
+		System.out.println(prop.getProperty("inthash"));
+		System.out.println(prop.getProperty("mac"));
+		System.out.println(prop.getProperty("macks"));
 	}
 	
 	Properties properties;
 	
-	public PropReader(String filepath) {
+	public PropReader(String filename) {
 		properties = new Properties();
 		try {
-			properties.load(getClass().getResourceAsStream(filepath));
+			properties.load(new FileInputStream(filename));
 		} catch (InvalidPropertiesFormatException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
