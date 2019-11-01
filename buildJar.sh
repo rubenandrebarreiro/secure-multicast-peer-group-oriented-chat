7102192ceef0aae160aad3d103f7f48744251e06
Eduardo Silva <emf.silva@campus.fct.unl.pt>
2019-11-01 23:26:30 +0000

    Add serlcert, update build script to sign the jar and update the jar

--- buildJar.sh
+++ buildJar.sh
@@ -2,3 +2,4 @@
 mvn clean package assembly:single
 mkdir -p SecureMChatClient-Application-Phase1
 mv target/*jar-with-dependencies*.jar ./SecureMChatClient-Application-Phase1/SecureMchatClient.jar
+jarsigner -keystore signKeystore SecureMChatClient-Application-Phase1/*.jar sign
